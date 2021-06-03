019c9761a2edcc9e3873b340df96deaa49989f7a
VLevin666 <79643502+VLevin666@users.noreply.github.com>
2021-06-03 12:16:05 +0300

    Update Driver.hpp

--- Driver.hpp
+++ Driver.hpp
@@ -10,9 +10,40 @@ private:
     unsigned int hoursPerDay[daysPersWeek];
 
 public:
+/** 
+	* @brief void setMaxWeight(double newMaxWeight)
+	*
+	* Setting max weight for the driver
+	*
+	* @param newMaxWeight Variable for setting the max weight
+	*/    
     void setMaxWeight(double newMaxWeight);
+    
+/** 
+	* @brief void setHoursPerDay(unsigned int newHoursPerDay[]
+	*
+	* Setting hours per day for the driver
+	*
+	* @param newHoursPerDay Variable for setting the hours per day
+	*/        
     void setHoursPerDay(unsigned int newHoursPerDay[]);
+    
+/** 
+	* @brief double getMaxWeight() const
+	*
+	* Getting max weight for the driver
+	* 
+	* @return MaxWeight-variable load capacity of the driver
+	*/        
     double getMaxWeight() const;
+    
+/** 
+	* @brief unsigned int getHoursForDay(unsigned int day) const
+	*
+	* Getting hours for day for the order
+	* 
+	* @return HoursForDay-variable is responsible for the work schedule of the driver
+	*/       
     unsigned int getHoursForDay(unsigned int day) const;
 
     bool operator==(const Driver &rhs) const;
