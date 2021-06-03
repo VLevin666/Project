a443e20c108721e49352a6e55e8101dfbbbe7d94
Korbendaiias <79076981+Korbendaiias@users.noreply.github.com>
2021-06-03 12:15:59 +0300

    Update DriversDB.hpp

--- DriversDB.hpp
+++ DriversDB.hpp
@@ -9,8 +9,40 @@ private:
     std::list<Driver> drivers;
 
 public:
+    
+    /**
+* @brief void add(const Driver &driver)
+*
+* Adding the driver to the database
+*
+* @param const Driver &driver-variable for adding the driver to the database
+*/
     void add(const Driver &driver);
+    
+    /**
+* @brief void remove(const Driver &driver)
+*
+* Removing the order to the database
+*
+* @param const Driver &driver-variable for removing the driver to the database
+*/
     void remove(const Driver &driver);
+    
+    /**
+* @brief Driver &operator[](unsigned int i)
+*
+* Getting the link to the i-th driver in drivers database
+*
+* @return a link to the i-th driver
+*/
     Driver &operator[](unsigned int i);
+    /**
+* @brief unsigned int getSize() const
+*
+* Getting number of the driver in drivers database
+*
+* @return Size-variable for getting the driver in drivers database
+*/
+   
     unsigned int getSize() const;
 };
